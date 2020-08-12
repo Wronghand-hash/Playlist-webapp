1cb70f702bb131457535aa066311f0c3f0d8bec4
Wronghand-hash <62405360+Wronghand-hash@users.noreply.github.com>
2020-08-12 16:26:44 +0330

    homepage changes
    contact me when you see this

--- webapp/settings.py
+++ webapp/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/assets/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
